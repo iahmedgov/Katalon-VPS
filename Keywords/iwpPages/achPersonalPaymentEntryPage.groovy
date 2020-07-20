5a329c66be37946f4b57edf5b2eb30626649570e
iahmed <iahmed@DESKTOP-B2QI6NS.govolution.com>
2020-07-20 10:21:24 -0400

    Created new class for achPersonalPaymentEntryPage

---
+++ Keywords/iwpPages/achPersonalPaymentEntryPage.groovy
@@ -0,0 +1,40 @@
+package iwpPages
+
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+
+import internal.GlobalVariable
+
+public class achPersonalPaymentEntryPage {
+	
+	
+	String Amount, UDF1, UDF2, UDF3, UDF4, UDF5, UDF6, UDF7, UDF8, UDF9, UDF10
+	String Fname, Lname, RTN, ACNumber, CACNumber, ACType, Date, AL1, AL2, ZIP, Email
+	
+	
+			@Keyword
+			def getDataACHPPM(int rowM, String dataFileM) 
+			{
+				
+				
+				
+				
+			}
+	
+	
+}
